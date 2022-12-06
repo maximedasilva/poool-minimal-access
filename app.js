9705b3a2aa3d4649429a8621592705265e9f63d1
Maxime Da Silva <maximed@poool.fr>
2022-12-06 14:32:28 +0100

    feat: add listeners

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-window.onload = () => {
+
+  window.onload = () => {
+  let i = 0;
+  let test = false;
+
+  const myFunc =  (i, event) => {
+    console.log(i, test)
+  }
   let script = document.createElement('script');
   script.src = "https://assets.poool.fr/access.js";
   script.async = true;
-  script.onload = () => {
-    const access = Access
-    .init('APP_ID')
-    .config({
+  script.onload = async () => {
+    let access = Access
+    .init('APP_ID');
+    access.createPaywall({
+      target: "#poool-widget",
+      content: ".content",
+      mode: "hide",
+      percent: 80,
+      pageType: "premium"
+    });
+    await access.destroy();
+    access.config({
+      debug: true,
       cookies_enabled: false,
     });
     access.createPaywall({
@@ -17,4 +33,22 @@ window.onload = () => {
     });
   }
   document.body.appendChild(script);
+  const coucouButton = document.getElementById('coucou');
+  const listenerButton = document.getElementById('listener');
+  const removeListenerButton = document.getElementById('removeButton');
+  const changeContext = document.getElementById('changeContext');
+
+  const loadPooolAccess = (t) => {
+    test = !t;
+    coucouButton.removeEventListener('click', myFunc)
+    coucouButton.addEventListener('click',myFunc);
+  }
+  listenerButton.addEventListener('click', () => {
+    loadPooolAccess(test)
+  })
+  removeListenerButton.addEventListener('click', () => {
+    console.log('heee');
+    coucouButton.removeEventListener('click', myFunc)
+  })
+  changeContext.addEventListener('click', () => {test = !test})
 };
